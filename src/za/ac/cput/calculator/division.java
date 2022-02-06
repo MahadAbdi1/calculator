06acb8e6627e0111be21f9940db77fabe222dc58
Mahad Hassan <fantasticcr2@gmail.com>
2022-02-06 18:55:28 +0200

    second commit

---
+++ src/za/ac/cput/calculator/division.java
@@ -0,0 +1,10 @@
+package za.ac.cput.calculator;
+
+public class division
+{
+    public static void main(String[] args) {
+       int a=10;
+       int b =5;
+        System.out.println( a-b);
+    }
+}
