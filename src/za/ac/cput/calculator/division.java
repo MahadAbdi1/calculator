448c2fb64eb5ed22fa2856ecd5c535ea8f468b4c
Mahad Hassan <fantasticcr2@gmail.com>
2022-02-07 17:16:59 +0200

    added something to playtestbrach

--- src/za/ac/cput/calculator/division.java
+++ src/za/ac/cput/calculator/division.java
@@ -4,11 +4,16 @@ package za.ac.cput.calculator;
 Student number : 219122822
 author Maha d Hassan
  */
+/*
+this playground brach
+ */
+
 public class division
 {
     public static void main(String[] args) {
        int a=10;
        int b =5;
         System.out.println( a-b);
+        System.out.println("another brach test");
     }
 }
